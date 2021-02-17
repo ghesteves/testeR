97f19157885b2673d60904f7cbdaf7a39f77480d
souzasam <spksouza@hotmail.com>
2021-02-17 10:07:48 -0300

    adding some comments.

--- R/fibonacci2.R
+++ R/fibonacci2.R
@@ -31,6 +31,29 @@ fibonacci <- function(n1=1, n2=5) {
   sequencia[n1:n2]
 }
 
+
+
+#' Applies and prints the golden ratio to each index(n by n-1) of a Fibonacci sequence
+#'
+#' This function print a Fibonacci sequence and a sequence of ratio between the n-th and index n-1
+#' according with the second argument that's determinate how many decimals places be needed to get the
+#' rounded gold number.
+#' This function show too how many ratios has been calculated for get a
+#' approximation of the gold number with 3 decimals places.
+#'
+#' This is just an initial test for constructing R packages using git and
+#' GitHub on-line.
+#'
+#' @param a is a positive integer number, the argument used for generate a Fibonacci sequence.
+#'
+#' @param b also a positive integer number, the argument used for determinate how many decimal
+#'           places needed for approximation.
+#'
+#' @return two vectors with the Fibonacci sequence and the gold ratio between n-th and the index n-1.
+#'
+#' @export
+#'
+
 goldnumber = function(a,b) {
 
   ## creating some variables
